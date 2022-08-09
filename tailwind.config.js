878dc4980b8b2ab1df06c5672f9eaa89a9bef5b8
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-08-09 20:18:00 +0700

    setting warna warna yang akan digunakan di tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "#002B5B",
+        grey: "#2B4865",
+        blue: "#256D85",
+        tosca: "#8FE3CF",
+      },
+    },
   },
   plugins: [],
 };
