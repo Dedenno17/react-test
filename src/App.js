b4c3031cfabb58240400169b35d2d26a83d0f59e
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-08-09 20:31:54 +0700

    menaruh link atau kit font-awesome untuk menggunakan icon free

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ function App() {
   return (
     <div>
       <p className="text-6xl text-darkBlue font-bold">Hello Reactjs</p>
+      <i class="fa-solid fa-eye text-4xl"></i>
+      <i class="fa-solid fa-eye-slash"></i>
+      <i class="fa-regular fa-circle-check text-tosca"></i>
     </div>
   );
 }
