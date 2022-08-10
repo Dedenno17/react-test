802bc6b219e08de6755e1867321e8933758f9a7c
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-08-10 07:51:31 +0700

    membernarkan error - error dalam Tabledata component

--- src/components/TableData/TableData.jsx
+++ src/components/TableData/TableData.jsx
@@ -8,35 +8,39 @@ const TableData = () => {
   return (
     <div className="w-full">
       <table className="border-collapse w-full">
-        <tr>
-          <th className="border-[1px] border-[#ddd] py-3 px-2 text-center bg-grey text-slate-100 w-[20%]">
-            ID No.
-          </th>
-          <th className="border-[1px] border-[#ddd] py-3 px-2 text-center bg-grey text-slate-100 w-[30%]">
-            Nama
-          </th>
-          <th className="border-[1px] border-[#ddd] py-3 px-2 text-center bg-grey text-slate-100 w-[35%]">
-            Alamat
-          </th>
-          <th className="border-[1px] border-[#ddd] py-3 px-2 text-center bg-grey text-slate-100 w-[15%]"></th>
-        </tr>
-        {DUMMY__DATA.map((item, i) => (
-          <tr
-            key={item.id}
-            className={
-              i % 2 === 0
-                ? "bg-[#f2f2f2] hover:bg-[#ddd]"
-                : "bg-slate-100 hover:bg-[#ddd]"
-            }
-          >
-            <td className="border-[1px] border-[#ddd] p-2">{item.id}</td>
-            <td className="border-[1px] border-[#ddd] p-2">{item.name}</td>
-            <td className="border-[1px] border-[#ddd] p-2">{item.alamat}</td>
-            <td className="border-[1px] border-[#ddd] p-2  text-center">
-              <i class="fa-solid fa-eye cursor-pointer"></i>
-            </td>
+        <thead>
+          <tr>
+            <th className="border-[1px] border-[#ddd] py-3 px-2 text-center bg-grey text-slate-100 w-[20%]">
+              ID No.
+            </th>
+            <th className="border-[1px] border-[#ddd] py-3 px-2 text-center bg-grey text-slate-100 w-[30%]">
+              Nama
+            </th>
+            <th className="border-[1px] border-[#ddd] py-3 px-2 text-center bg-grey text-slate-100 w-[35%]">
+              Alamat
+            </th>
+            <th className="border-[1px] border-[#ddd] py-3 px-2 text-center bg-grey text-slate-100 w-[15%]"></th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {DUMMY__DATA.map((item, i) => (
+            <tr
+              key={item.id}
+              className={
+                i % 2 === 0
+                  ? "bg-[#f2f2f2] hover:bg-[#ddd]"
+                  : "bg-slate-100 hover:bg-[#ddd]"
+              }
+            >
+              <td className="border-[1px] border-[#ddd] p-2">{item.id}</td>
+              <td className="border-[1px] border-[#ddd] p-2">{item.name}</td>
+              <td className="border-[1px] border-[#ddd] p-2">{item.alamat}</td>
+              <td className="border-[1px] border-[#ddd] p-2  text-center">
+                <i className="fa-solid fa-eye cursor-pointer"></i>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
