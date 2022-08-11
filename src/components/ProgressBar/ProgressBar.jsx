d08f698b9587ffd16b68bfeeea42c39ac7889a29
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-08-11 16:03:01 +0700

    membuat utility class shadow grey & menambahkannya ke progress bar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,20 +1,18 @@
+const pages = [1, 2, 3, 4];
+
 const ProgressBar = () => {
   return (
     <div className="w-[90%] bg-slate-100 h-1 mx-auto relative lg:w-[75%]">
       <span className="block w-full h-full bg-tosca"></span>
       <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-between items-center">
-        <span className="py-[2px] px-[10px] bg-grey text-slate-100 border-[1px] border-slate-100 rounded-full">
-          1
-        </span>
-        <span className="py-[2px] px-[10px] bg-grey text-slate-100 border-[1px] border-slate-100 rounded-full">
-          2
-        </span>
-        <span className="py-[2px] px-[10px] bg-grey text-slate-100 border-[1px] border-slate-100 rounded-full">
-          3
-        </span>
-        <span className="py-[2px] px-[10px] bg-grey text-slate-100 border-[1px] border-slate-100 rounded-full">
-          4
-        </span>
+        {pages.map((item, i) => (
+          <span
+            className="py-[2px] px-[10px] bg-grey text-slate-100 border-[1px] border-slate-100 rounded-full shadow-grey"
+            key={i}
+          >
+            {item}
+          </span>
+        ))}
       </div>
     </div>
   );
