5dd357b581029cff396310e227088b53c00590f0
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-08-11 14:34:25 +0700

    menginstall redux react-redux & redux toolkit serta membuat file store dan mewrap app dengan provider compoennt

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import Store from "./app/store";
+
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
